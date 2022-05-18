e4e0848e26ffac3395552eb8e72451de10636ad5
Crkf61 <cjl95@cam.ac.uk>
2022-05-18 15:36:33 +0100

    adjust display code for circular problem

--- 2D_problem/pygame_utils.py
+++ 2D_problem/pygame_utils.py
@@ -1,29 +1,27 @@
 import pygame
 import sys
 
-HEIGHT = 800
-WIDTH = 1200
-box_height = 100
+SIZE = 800
 
 background_colour = (110,170,120)
 dot_colour = (90,30,30)
-box_colour = (250,250,250)
+shell_colour = (250,250,250)
 dot_rad = 30  # radius in pixels
 
 
 def start_display():
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen = pygame.display.set_mode((SIZE, SIZE))
     clock = pygame.time.Clock()
     screen.fill(background_colour)
     return screen, clock
 
 def draw_positions(positions, screen, length):
-    # draw rectangle for 'electrons' to bounce inside
-    box = (0,(HEIGHT - box_height)/2,WIDTH,box_height)
-    pygame.draw.rect(screen, box_colour, box)
+    # draw circle for 'electrons' to bounce inside
+    RAD = SIZE/2
+    pygame.draw.circle(screen, shell_colour, (RAD, RAD), RAD)
 
     for pos in positions:
-        screen_pos = (int(WIDTH * pos/ length), HEIGHT/2)
+        screen_pos = np.round(RAD * pos)
         pygame.draw.circle(screen, dot_colour, screen_pos, dot_rad)
 
     pygame.display.update()
